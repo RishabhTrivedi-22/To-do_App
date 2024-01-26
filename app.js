058071f2f6c571d163c789998be3ed9acf50a65b
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-01-26 14:37:06 +0530

    Basic Logic added

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ const d = document;
 
 const wrapper = d.querySelector(".wrapper");
 const categorySelect = d.querySelectorAll(".category");
-const backBtn = d.querySelectorAll(".back-btn")
+const backBtn = d.querySelectorAll(".back-btn");
+const addBtn = d.querySelector(".add-btn");
+const cancelBtn = d.querySelector(".cancel-btn");
+const addTaskMenu = d.querySelector(".add-task-wrapper");
+const blackBackDrop = d.querySelector(".black-backdrop");
 
 const toggleMenu = () => {
   wrapper.classList.toggle("show-category");
 };
 
 backBtn.forEach(element => {
-  element.addEventListener("click", toggleMenu)
-})
-categorySelect.forEach(element => {
-  element.addEventListener("click", toggleMenu)
+  element.addEventListener("click", toggleMenu);
 });
+categorySelect.forEach(element => {
+  element.addEventListener("click", toggleMenu);
+});
+
+const toggleAddForm = () => {
+  addTaskMenu.classList.toggle("active");
+  blackBackDrop.classList.toggle("active");
+  addBtn.classList.toggle("active");
+
+};
+
+addBtn.addEventListener("click", toggleAddForm);
+cancelBtn.addEventListener("click", toggleAddForm)
